d40ca7b0c20585f1f52396beb7ab01b0c95d24b1
ZC-Huang <60633966+ZC-Huang@users.noreply.github.com>
2022-01-23 20:19:13 -0800

    Add "About Me" Button

--- app/src/main/java/edu/neu/numad22sp_zichenghuang/MainActivity.java
+++ app/src/main/java/edu/neu/numad22sp_zichenghuang/MainActivity.java
@@ -2,7 +2,12 @@ package edu.neu.numad22sp_zichenghuang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get button
+        Button btn = findViewById(R.id.buttonReadMe);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MyAPP", "exampleMsg");
+                Toast.makeText(getApplicationContext(), "Name: Zicheng Huang\nEmail: huang.ziche@northeastern.edu", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
     }
 }
