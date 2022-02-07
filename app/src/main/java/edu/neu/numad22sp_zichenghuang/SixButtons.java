57d7cf7d60464fcabee285e73612174d40b8af29
ZC-Huang <60633966+ZC-Huang@users.noreply.github.com>
2022-02-06 16:20:27 -0800

    Button and Change Text
    completed setting the buttons to change textview

--- app/src/main/java/edu/neu/numad22sp_zichenghuang/SixButtons.java
+++ app/src/main/java/edu/neu/numad22sp_zichenghuang/SixButtons.java
@@ -3,6 +3,9 @@ package edu.neu.numad22sp_zichenghuang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SixButtons extends AppCompatActivity {
 
@@ -10,5 +13,60 @@ public class SixButtons extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_six_buttons);
+
+        changeTextViewOnButtonClick();
+    }
+
+    //change textview on button click
+    private void changeTextViewOnButtonClick() {
+        TextView changingText = (TextView) findViewById(R.id.textView2);
+        Button changeTextButtonA = (Button) findViewById(R.id.buttonA);
+        Button changeTextButtonB = (Button) findViewById(R.id.buttonB);
+        Button changeTextButtonC = (Button) findViewById(R.id.buttonC);
+        Button changeTextButtonD = (Button) findViewById(R.id.buttonD);
+        Button changeTextButtonE = (Button) findViewById(R.id.buttonE);
+        Button changeTextButtonF = (Button) findViewById(R.id.buttonF);
+
+        changeTextButtonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changingText.setText("Pressed: A");
+            }
+        });
+
+        changeTextButtonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changingText.setText("Pressed: B");
+            }
+        });
+
+        changeTextButtonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changingText.setText("Pressed: C");
+            }
+        });
+
+        changeTextButtonD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changingText.setText("Pressed: D");
+            }
+        });
+
+        changeTextButtonE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changingText.setText("Pressed: E");
+            }
+        });
+
+        changeTextButtonF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changingText.setText("Pressed: F");
+            }
+        });
     }
 }
